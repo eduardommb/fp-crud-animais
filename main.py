fe060a7f6af30fdd8fe6bcca688d4ac74749b6c0
LarissaGiovanna <lgsc@cesar.school>
2025-11-18 11:46:18 -0300

    começo rotas e integracao pags html

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-## ola mundo.
+from flask import Flask, render_template, url_for
+
+#inicializacao
+app = Flask(__name__)
+
+pag = "/outra"
+
+#rotas
+@app.route("/") #caminho principal (home)
+def teste():
+    return render_template('index.html', pag=pag)
+
+@app.route("/outra")
+def outra_pag():
+    return "teste" 
+
+
+#execucao
+app.run(debug="true")
