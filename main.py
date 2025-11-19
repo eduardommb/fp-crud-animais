daa874a1c04c295a6e22b8b540883950e9cfdd07
LarissaGiovanna <lgiova544@gmail.com>
2025-11-18 21:43:30 -0300

    criação html pagina read crud_pets - organizado as variaveis, import das fontes e config padroes no arquivo root.css - organizado rotas das pags index e crud_pets - iniciado css da pag crud_pets

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from flask import Flask, render_template, url_for
 #inicializacao
 app = Flask(__name__, static_folder='static')
 
-pag = "/outra"
+crud_pets = 'crud_pets'
+crud_adotantes = 'crud_adotantes'
 
 #rotas
 @app.route("/") #caminho principal (home)
-def teste():
-    return render_template('index.html', pag=pag)
+def ir_home():
+    return render_template('index.html', crud_pets = crud_pets, crud_adotantes = crud_adotantes)
 
-@app.route("/outra")
-def outra_pag():
-    return "teste" 
+@app.route("/crud_pets")
+def ir_crud_pets():
+    return render_template('crud_pets.html')
 
+@app.route('/crud_adotantes')
+def ir_crud_adotantes():
+    return render_template('crud_adotantes.html')
 
 #execucao
 app.run(debug="true")
